e7689f1e865a5c89546e54ddb2fc03df8fdf51ac
Neil Gallagher <nmg14@duke.edu>
2020-08-14 11:49:33 -0400

    updated get_X

--- data_tools.py
+++ data_tools.py
@@ -282,11 +282,15 @@ def combine_data(*args):
         return tuple(features)
 
 
-def get_X(weights, featureList):
-    normFeat = [f / np.sqrt(np.mean(f**2)) for f in featureList] 
-    weightedFeat = [f*w for f,w in zip(normFeat, weights)]
-    X = np.concatenate(weightedFeat, axis=1)
-    return X
+def get_X(weights, feature_list, return_weights=False):
+    rms = [np.sqrt(np.mean(f**2)) for f in feature_list]
+    new_weights = weights / rms
+    weighted_feat = [f*w for f,w in zip(feature_list, new_weights)]
+    X = np.concatenate(weighted_feat, axis=1)
+    if return_weights:
+        return (X, new_weights)
+    else:
+        return X
 
 
 def get_weights(group_list, subset_idx=None):
