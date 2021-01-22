4a0b2e8228e24a7934ff047514fc2ab14e7433d0
Jack <12833246+jackgoffinet@users.noreply.github.com>
2021-01-22 13:38:55 -0500

    made a GUI for saveFeatures.m

--- saveFeatures.m
+++ saveFeatures.m
@@ -1,18 +1,13 @@
 function saveFeatures(saveFile, options)
 % estimate spectral features
+%
+% In previous versions, there was an additional input parameter called
+% "options." In this version a GUI pops up and asks for these options from
+% the user.
+%
 % INPUTS
 % saveFile: name of '.mat' file containing the data and labels
 %   variables. And to which the processed data will be saved
-% options: structure of options for running this function
-% FIELDS
-%   windowOpts: (Optional) a binary vector with length=number of windows,
-%     determining which windows to analyze for features.
-%   featureList: (Optional) Cell array of strings indicating which
-%     features to calculate
-%   parCores: (Optional) integer indicating number of cores to use for
-%     parallel computing. If 0, all tasks executed in serial.
-%   mvgcFolder: needed if featureList includes 'granger'. String
-%     indicating folder containing MVGC toolbox.
 %
 % LOADED VARIABLES
 % labels: Structure containing labeling infomation for data
@@ -30,7 +25,7 @@ function saveFeatures(saveFile, options)
 %   dataSegments method.
 %
 % SAVED VARIABLES
-% power: MxNxP matrix of power values where M is frequency, N is brain area, 
+% power: MxNxP matrix of power values where M is frequency, N is brain area,
 %     and P is time window
 % coherence: MxNxPxQ array to store coherence variables where M is frequency,
 %     N is time window, and P and Q are the two brain areas where coherence is
@@ -57,10 +52,7 @@ function saveFeatures(saveFile, options)
 %       features represented in instArray. P iterates over
 %       undirected pairs of regions, F iterates over frequencies.
 WELCH_WIN_LEN = 1/4; % quarter of a second (frequency resolution of 4Hz)
-  
-%% Get options from GUI
 
-% GUI here
 
 if nargin < 2
     % fill with default parameters
@@ -69,10 +61,19 @@ end
 options=fillDefaultOpts(options);
 
 
+%% Get options from GUI
+myGui = gui();
+while isvalid(myGui);
+    options = myGui.getOptions();
+    pause(0.001);
+end 
+
+
 %% load data and prep for feature generation
 fprintf('Ignore the following warning(s) \n')
 load(saveFile,'X','dataSegments','labels', 'windowTimes')
 fs = labels.fs;
+
 
 % evaluate at every integer frequency up to nyquist
 f = 1:floor(fs/2);
@@ -93,7 +94,7 @@ if ~isempty(options.windowOpts)
     X=X(:,:,myIdx);
     windowLabels = fieldnames(labels.windows);
     for m = 1:numel(windowLabels)
-        labels.windows.(windowLabels{m}) = labels.windows.(windowLabels{m})(myIdx); 
+        labels.windows.(windowLabels{m}) = labels.windows.(windowLabels{m})(myIdx);
     end
 end
 
@@ -101,7 +102,7 @@ end
 % area and then by frequency
 [N,C,W] = size(X);
 xReshaped = reshape(X, N, C*W);
-windowSize = round(fs*WELCH_WIN_LEN); 
+windowSize = round(fs*WELCH_WIN_LEN);
 
 fStrings = compose('%d', f)';
 %% get power spectrum
@@ -115,21 +116,21 @@ if any(ismember('power', options.featureList)) && ...
         scale = 'psd';
     end
     power = pwelch(xReshaped, windowSize,[], f,fs, scale);
-    % Reshape to MxNxP matrix where M is frequency, N is brain area, and P is time window 
+    % Reshape to MxNxP matrix where M is frequency, N is brain area, and P is time window
     power = reshape(power, [], C, W);
     power = single(abs(power));
-    
+
     % generate/save matrix where elements name corresponding
-    % feature in power matrix  
+    % feature in power matrix
     freqMat = repmat(fStrings, [1 C]);
     areaMat = repmat(labels.area', [nFreq 1]);
     labels.powerFeatures = string(cellfun(@(x,y) [x ' ' y], areaMat, freqMat, ...
         'UniformOutput', false));
-    
+
     labels.powVersion = options.version.power;
-    
+
     save(saveFile, 'power', '-append')
-    
+
 elseif any(ismember('power', options.featureList)) && ...
         ~any(ismember('causality', options.featureList))
     error(['Power cannot be calculated using version %s without adding ''causality'' to'...
@@ -146,22 +147,22 @@ if any(ismember('coherence',options.featureList))
     % initialize feature labels to empty strings
     cohFeatures = cell(nFreq,C,C);
     cohFeatures(:) = {''};
-    
+
     if options.parCores, pp = parpool([2 options.parCores]); end
     a = tic;
     for c1=1:C
         x = squeeze(X(:, c1, :));
-        
+
         areaList = labels.area;
         parfor (c2=(c1+1):C, options.parCores)
             y = squeeze(X(:, c2, :));
-            
+
             % use coherence function to calculate coherence between each pair of
             % channels; save to each brain pair location (ie X and Y as well as Y
             % and X)
             cxy = mscohere(x,y, windowSize,[], f,fs);
             coherence(:,:,c2,c1) = cxy; % symmetric
-            
+
             % save feature labels for this pair of regions
             cohFeatures(:,c2,c1) = cellfun(@(x) [areaList{c1} '-' areaList{c2} ' ' x], fStrings, ...
                 'UniformOutput', false);
@@ -170,11 +171,11 @@ if any(ismember('coherence',options.featureList))
         fprintf('Evaluated remaining %s coherence(s): %2.1fs elapsed\n', areaList{c1}, toc(a))
     end
     if options.parCores, delete(pp), end
-    
+
     labels.cohFeatures = string(cohFeatures);
-    
+
     labels.cohVersion = options.version.coherence;
-    
+
     coherence=single(abs(coherence));
     % save coherence results to data
     save(saveFile, 'coherence', '-append')
@@ -185,7 +186,7 @@ end
 if any(ismember('granger', options.featureList))
     mvgcStartupScript = [options.mvgcFolder '/startup.m'];
     run(mvgcStartupScript)
-    
+
     % only high pass filter data for versions <1.5
     if strcmp(options.version.granger, 'saveFeatures_1.5')
         X_filt = double(X);
@@ -194,7 +195,7 @@ if any(ismember('granger', options.featureList))
             'StopbandFrequency', 1/labels.windowLength , 'SampleRate', fs);
         X_filt = filtfilt(d, double(X));
     end
-    
+
     % generate Granger causality values matrix in the form MxPxQ, where M
     % iterates over pairs of brain regions, P is frequency, and Q is time
     % window.
@@ -204,10 +205,10 @@ if any(ismember('granger', options.featureList))
     labels.gcFeatures = string(gcFeatures);
     instant = single(instant);
     labels.instFeatures = string(instFeatures);
-    
+
     labels.gcVersion = options.version.granger;
-    
-    save(saveFile, 'granger', 'instant', '-append') 
+
+    save(saveFile, 'granger', 'instant', '-append')
 end
 
 %% Get linear causality features
@@ -215,8 +216,8 @@ end
 if any(ismember('causality', options.featureList))
     mvgcStartupScript = [options.mvgcFolder '/startup.m'];
     run(mvgcStartupScript)
-    
-    % generate additive Granger causality values matrix in the form MxPxQ, where M 
+
+    % generate additive Granger causality values matrix in the form MxPxQ, where M
     % iterates over pairs of brain regions, P is frequency, and Q is time
     % window.
     X = double(X);
@@ -224,44 +225,44 @@ if any(ismember('causality', options.featureList))
         options.parCores);
     causality = single(causality);
     labels.causFeatures = string(causFeatures);
-    
+
     labels.causVersion = options.version.causality;
-    
+
     if any(ismember('power', options.featureList)) && ...
             strcmp(options.version.power, 'caus_saveFeatures_1.5')
         power = zeros(nFreq, C, W);
         for k =1:C
             power(:,k,:) = S(k,k,:,:);
         end
-        
+
         % generate/save matrix where elements name corresponding
         % feature in power matrix
         freqMat = repmat(fStrings, [1 C]);
         areaMat = repmat(labels.area', [nFreq 1]);
         labels.powerFeatures = string(cellfun(@(x,y) [x ' ' y], areaMat, freqMat, ...
         'UniformOutput', false));
-    
+
         labels.powVersion = options.version.power;
-    
+
         save(saveFile, 'causality','power','-append')
     else
-        save(saveFile, 'causality','-append') 
+        save(saveFile, 'causality','-append')
     end
 end
 
 %% Take Fourier transform of data
 if any(ismember('fourier', options.featureList))
     Ns = ceil(N/2);
-    if strcmp(opts.version.fft, '1.1')
+    if strcmp(options.version.fft, '1.1')
         scale = 1/sqrt(N);
     else
         scale = 1/N;
     end
     xFft = scale*fft(double(X));
     xFft = 2*(xFft(2:Ns+1,:,:));
-    
+
     labels.fftVersion = options.version.fft;
-    
+
     save(saveFile,'xFft','-append');
 end
 
